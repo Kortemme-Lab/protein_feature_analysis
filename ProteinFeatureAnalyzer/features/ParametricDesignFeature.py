Fix a bug in folding angle calculation.

ProteinFeatureAnalyzer/features/ParametricDesignFeature.py
@@ -132,16 +132,19 @@ class ParametricDesignFeature(Feature):
             'cylinder_strand_angle' : row[2],
             'folding_angle' : row[3]})
 
-  def visualize_beta_sheet_features(self, feature, fig_save_path=None):
+  def visualize_beta_sheet_features(self, feature, rmsd_cutoff=float('inf'), fig_save_path=None):
     '''Visualized the beta sheet features features.'''
     
     def get_data(feature):
       if feature == 'cylinder_curvature':
-          return [1 / d['cylinder_radius'] for d in self.beta_sheet_features]
+          return [1 / d['cylinder_radius'] for d in self.beta_sheet_features 
+                  if d['cylinder_fitting_rmsd'] < rmsd_cutoff]
       elif feature in ['cylinder_strand_angle', 'folding_angle']:
-          return [np.degrees(d[feature]) for d in self.beta_sheet_features]
+          return [np.degrees(d[feature]) for d in self.beta_sheet_features
+                  if d['cylinder_fitting_rmsd'] < rmsd_cutoff]
       else:
-          return [d[feature] for d in self.beta_sheet_features]
+          return [d[feature] for d in self.beta_sheet_features
+                  if d['cylinder_fitting_rmsd'] < rmsd_cutoff]
     
     data = get_data(feature)
 

ProteinFeatureAnalyzer/features/secondary_structures.py
@@ -506,7 +506,13 @@ class BetaSheet(SecondaryStructure):
 
       if s_half > 1: continue
 
-      folding_angle = 2 * np.arcsin(s_half)
+      folding_angle = np.arcsin(s_half)
+      
+      #print("Fitting RMSD =", cylinder_fitting.fitting_rmsd(w_fit, C_fit, r_fit, cas)) ###DEBUG
+      #print("Radius =", r_fit)###DEBUG
+      #print("Arc =", max(arc_strand, arc_pair))
+      #print("Folding_anlge =", folding_angle)###DEBUG
+      #cylinder_fitting.show_fit(w_fit, C_fit, r_fit, cas) ###DEBUG
 
       self.graph.node[res]['cylinder_radius'] = r_fit
       self.graph.node[res]['cylinder_strand_angle'] = geometry.angle(w_fit, strand_direction)