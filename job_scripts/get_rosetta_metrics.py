0cb284536d13e2b14f5dd9176f00c3255078c6f8
xingjiepan <xingjiepan@gmail.com>
2018-09-07 13:57:58 -0700

    Update the get_rosetta_metrics.py script

--- job_scripts/get_rosetta_metrics.py
+++ job_scripts/get_rosetta_metrics.py
@@ -133,19 +133,29 @@ def get_holes_score_data(pose, structure_name):
 def calc_buried_np_SASA(pose, residue_types=None):
     '''Calculate the buried nonpolar surface area in the designed
     structure on for given residue types'''
+    # Calculate the hydrophobic SASA
+    
     rsd_sasa = pyrosetta.rosetta.utility.vector1_double()
     rsd_hydrophobic_sasa = pyrosetta.rosetta.utility.vector1_double()
     rosetta.core.scoring.calc_per_res_hydrophobic_sasa(pose, rsd_sasa, rsd_hydrophobic_sasa, 1.4) #The last arguement is the probe radius
 
+    # Calculate the hydrophobic buried SASA
+
+    for i in range(1, pose.size() + 1):
+        total_sasa = rosetta.core.scoring.normalizing_area_total_hydrophobic_atoms_only(pose.residue(i).name1())
+        rsd_hydrophobic_sasa[i] = total_sasa - rsd_hydrophobic_sasa[i]
+
+    # Sum the SASA of given residues
+
     if residue_types:
-        afilmvwy_residues = [i for i in range(1, pose.size() + 1) if pose.residue(i).name1() in residue_types]
-        return sum(rsd_hydrophobic_sasa[i] for i in afilmvwy_residues)
+        residues = [i for i in range(1, pose.size() + 1) if pose.residue(i).name1() in residue_types]
+        return sum(rsd_hydrophobic_sasa[i] for i in residues)
     
     return sum(rsd_hydrophobic_sasa)
 
 def get_buried_np_SASA_per_res_data(pose, structure_name, residue_types=None):
     '''Get the buried non-polar surface area divided by the total number of residues.'''
-    return [(structure_name, calc_buried_np_SASA(pose) / pose.size())]
+    return [(structure_name, calc_buried_np_SASA(pose, residue_types=residue_types) / pose.size())]
 
 def get_atomic_contact_data(pose, structure_name):
     '''Get the atomic contact number for a pose.'''
